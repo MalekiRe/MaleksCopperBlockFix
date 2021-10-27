19ca6d60f7269a7e4f17c63e65c84c50dd403e86
MalekiRe <pocmalek@gmail.com>
2021-10-26 19:19:46 -0700

    water now causes oxidiation to happen faster, and also causes oxidiazation period

--- src/main/java/net/malek/copperblockfix/mixin/OxidizableBlockMixin.java
+++ src/main/java/net/malek/copperblockfix/mixin/OxidizableBlockMixin.java
@@ -1,8 +1,6 @@
 package net.malek.copperblockfix.mixin;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Oxidizable;
-import net.minecraft.block.OxidizableBlock;
+import net.minecraft.block.*;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.state.property.Property;
 import net.minecraft.util.math.BlockPos;
@@ -12,6 +10,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Properties;
 import java.util.Random;
 
 @Mixin(OxidizableBlock.class)
@@ -21,16 +20,22 @@ public class OxidizableBlockMixin {
 	@Inject(at = @At("HEAD"), method = "randomTick", cancellable = true)
 	private void randomTickMixin(BlockState state, ServerWorld world, BlockPos pos, Random random, CallbackInfo info) {
 		for(Direction direction : Direction.values()) {
-			if(world.getBlockState(pos.offset(direction)).isAir()) {
+			if(world.getBlockState(pos.offset(direction)).isAir() || world.getBlockState(pos.offset(direction)).getBlock() instanceof Waterloggable || world.getBlockState(pos.offset(direction)).getBlock() == Blocks.WATER) {
 				if (random.nextFloat() < 0.05688889F) {
-					if(Oxidizable.getIncreasedOxidationBlock(state.getBlock()).isPresent()) {
-						BlockState state1 = Oxidizable.getIncreasedOxidationBlock(state.getBlock()).get().getDefaultState();
-						for(Property property : state.getProperties())
-						{
-							state1 = state1.with(property, state.get(property));
-						}
-						world.setBlockState(pos, state1);
+					int big = 1;
+					if(world.getBlockState(pos.offset(direction)).getBlock() instanceof Waterloggable || world.getBlockState(pos.offset(direction)).getBlock() == Blocks.WATER) {
+						big = 2;
 					}
+					for(int i = 0; i < big; i++) {
+						if (Oxidizable.getIncreasedOxidationBlock(state.getBlock()).isPresent()) {
+							BlockState state1 = Oxidizable.getIncreasedOxidationBlock(state.getBlock()).get().getDefaultState();
+							for (Property property : state.getProperties()) {
+								state1 = state1.with(property, state.get(property));
+							}
+							world.setBlockState(pos, state1);
+						}
+					}
+
 				}
 				return;
 			}
